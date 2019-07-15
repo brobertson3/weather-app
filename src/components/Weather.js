44159e49a686ad2b2e28386b7a2d5ced84f74cb7
brobertson3 <brobertson3@users.noreply.github.com>
2019-07-14 20:32:45 -0700

    Main changes to the styling

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,23 +2,53 @@ import React from "react";
 import sunIcon from "../icons/sun.svg";
 import downArrow from "../icons/down-arrow.svg";
 const Weather = props => {
+  const returnTempColor = temp => {
+    if (temp < 50) {
+      return "cold";
+    } else if (temp >= 50 && temp < 75) {
+      return "moderate";
+    } else if (temp >= 75 && temp < 90) {
+      return "warm";
+    } else return "hot";
+  };
+
   return (
     <div className="single-day-weather-condition-container">
-      <p>
+      <p className="city-container">
         {props.city}, {props.country}
       </p>
-      <p>Today</p>
+      {/* <p>Today</p> */}
       <div className="weather-icon-container weather-split-container">
         <img id="weather-condition-icon" src={sunIcon} alt="weather icon" />
       </div>
       <div className="weather-details-container weather-split-container">
-        <p>Current: {props.currentTemp}&deg;</p>
+        <p>
+          <span
+            className={`temperature-numbers current-temp-number ${returnTempColor(
+              props.currentTemp
+            )}`}
+          >
+            {props.currentTemp}&deg;
+          </span>
+        </p>
         <div className="high-low-container">
           <img className="high-temp-icon" src={downArrow} alt="up arrow" />
-          <span>{props.highTemp}&deg;</span>
+          <span
+            className={`temperature-numbers high-temp-number ${returnTempColor(
+              props.highTemp
+            )}`}
+          >
+            {props.highTemp}&deg;
+          </span>
 
-          <img src={downArrow} alt="down arrow" />
-          <span>{props.lowTemp}&deg;</span>
+          <img className="low-temp-icon" src={downArrow} alt="down arrow" />
+          <span
+            className={`temperature-numbers low-temp-number ${returnTempColor(
+              props.lowTemp
+            )}`}
+          >
+            {props.lowTemp}&deg;
+          </span>
         </div>
         <p>{props.description}</p>
       </div>
